91815849097afdc4043966ea75878877e5b5ba83
Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>
2023-10-16 20:07:20 +0200

    Ahmed Awad
    Signed-off-by: Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>

--- get_width.c
+++ get_width.c
@@ -2,6 +2,10 @@
 
 /**
  * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
@@ -30,4 +34,3 @@ int get_width(const char *format, int *i, va_list list)
 
 	return (width);
 }
-
