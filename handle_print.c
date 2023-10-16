31e1a00ebc04258e8ef8f62b52b447d237a18a2e
Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>
2023-10-16 20:09:08 +0200

    Ahmed Awad
    Signed-off-by: Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>

--- handle_print.c
+++ handle_print.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument by its type
- * @fmt: The Formatted string.
- * @list: List of arguments wants to be printed.
- * @ind: ind an int.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
  * @width: get width.
@@ -47,4 +47,3 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	}
 	return (printed_chars);
 }
-
